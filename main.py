6e0f4bf073e8b2ffe06583b4460ffd5ef977738c
Sebastian Gutierrez <sebsgtzmtz0109@gmail.com>
2025-07-22 12:20:39 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import streamlit.components.v1 as components
+
+
+def main():
+    st.title("📄 Embedded HTML Example")
+
+    with open("index.html", "r", encoding='utf-8') as f:
+        html_str = f.read()
+
+    st.html(html_str)
+
+
+if __name__ == "__main__":
+    main()
